dd78ad16bc23fe5cb30716c521c0d51b5ad72596
tangxl <1092678514@qq.com>
2023-07-06 11:28:51 +0800

    国际化

--- client/packages/openblocks/src/i18n/index.tsx
+++ client/packages/openblocks/src/i18n/index.tsx
@@ -2,8 +2,14 @@ import { getI18nObjects, Translator } from "openblocks-core";
 import * as localeData from "./locales";
 import { I18nObjects } from "./locales/types";
 
-export const { trans, transToNode, language } = new Translator<typeof localeData.en>(
+let { trans, transToNode, language } = new Translator<typeof localeData.en>(
   localeData,
   REACT_APP_LANGUAGES
 );
+language = 'zh';
+export {
+  trans, 
+  transToNode,
+  language
+}
 export const i18nObjs = getI18nObjects<I18nObjects>(localeData, REACT_APP_LANGUAGES);
--- client/packages/openblocks/src/i18n/locales/zh.ts
+++ client/packages/openblocks/src/i18n/locales/zh.ts
@@ -1,3 +1,7 @@
 import { en } from "./en";
 
-export const zh: typeof en | undefined = undefined;
+// export const zh: typeof en | undefined = undefined;
+export const zh = {
+    ...en,
+    productName: "鹏腾"
+}
